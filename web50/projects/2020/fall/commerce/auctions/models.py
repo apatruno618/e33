eb0c9c0cc2f3790b2ad118a452147cc186a9b0f4
Anna Patruno <apatruno618@gmail.com>
2020-10-10 11:15:11 -0400

    models

--- web50/projects/2020/fall/commerce/auctions/models.py
+++ web50/projects/2020/fall/commerce/auctions/models.py
@@ -4,3 +4,27 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=200)
+    starting_bid = models.DecimalField(max_digits=8, decimal_places=2)
+    photo_link = models.URLField()
+    category = models.CharField(max_length=64)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField()
+
+
+class Bid(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing_id = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    bid = models.DecimalField(max_digits=8, decimal_places=2)
+    date_created = models.DateTimeField()
+
+
+class Comment(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing_id = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=200)
+    date_created = models.DateTimeField()
