aa3bf630f202dc255c6cd9b9d2ab9101063bb131
Anna Patruno <apatruno618@gmail.com>
2020-11-23 21:47:37 -0500

    customer model, admin app, index view

--- web50/projects/2020/fall/finalproject/tropicanna/models.py
+++ web50/projects/2020/fall/finalproject/tropicanna/models.py
@@ -6,3 +6,10 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=100)
     phone = models.IntegerField()
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "phone": self.phone
+        }
--- web50/projects/2020/fall/finalproject/tropicanna/views.py
+++ web50/projects/2020/fall/finalproject/tropicanna/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from .models import Customer
+
 # Create your views here.
+
+
+def index(request):
+    return render(request, "index.html", {
+        "customers": Customer.objects.all()
+    })
