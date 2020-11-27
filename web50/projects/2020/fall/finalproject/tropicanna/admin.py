46db0455e40551bb1cd92567e6eb243801465621
Anna Patruno <apatruno618@gmail.com>
2020-11-27 10:56:26 -0500

    Final project models, views, templates
    Models
    - User, Customer, Category, Flavor, Product
    Views
    - index, register, login, logout logic
    - index view displays customers and products
    Templates
    - index, register, login, logout
    - bootstrap added
    - nav bar
    - layout.html

--- web50/projects/2020/fall/finalproject/tropicanna/admin.py
+++ web50/projects/2020/fall/finalproject/tropicanna/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Customer
+from .models import Customer, Category, Flavor, Product
 
 
 admin.site.register(Customer)
+admin.site.register(Category)
+admin.site.register(Flavor)
+admin.site.register(Product)
--- web50/projects/2020/fall/finalproject/tropicanna/models.py
+++ web50/projects/2020/fall/finalproject/tropicanna/models.py
@@ -1,6 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class User(AbstractUser):
+    pass
 
 
 class Customer(models.Model):
@@ -12,4 +16,40 @@ class Customer(models.Model):
             "id": self.id,
             "name": self.name,
             "phone": self.phone
+        }
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "price": self.price
+        }
+
+
+class Flavor(models.Model):
+    name = models.CharField(max_length=100)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        "Category", on_delete=models.CASCADE, related_name="products")
+    flavor = models.ForeignKey(
+        "Flavor", on_delete=models.CASCADE, related_name="products")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "category": self.category,
+            "flavor": self.flavor
         }
